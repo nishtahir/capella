63a7a2a61061a901804a97d2bb4bd277ce630ec0
Nish Tahir <nishtahir@outlook.com>
2019-04-09 19:49:21 -0400

    Add deep sleep and https connectivity

--- hardware/src/main.cpp
+++ hardware/src/main.cpp
@@ -3,11 +3,15 @@
 #include <ESP8266HTTPClient.h>
 #include "ArduinoJson.h"
 
-int ANALOG_SENSOR = A0;
-int MOISTURE_MIN = 1024;
-int MOISTURE_MAX = 300;
-int PERCENT_MIN = 0;
-int PERCENT_MAX = 100;
+const int ANALOG_SENSOR = A0;
+const int MOISTURE_MIN = 1024;
+const int MOISTURE_MAX = 300;
+const int PERCENT_MIN = 0;
+const int PERCENT_MAX = 100;
+
+const int PORT = 443;
+
+const int SLEEP_SECONDS = 1800;
 
 void startSerial()
 {
@@ -40,6 +44,8 @@ void startWifi()
 
 void setup()
 {
+    pinMode(D0, WAKEUP_PULLUP);
+
     startSerial();
     startWifi();
 }
@@ -67,28 +73,36 @@ void loop()
 
     String buffer;
     serializeJson(object, buffer);
-    Serial.println(buffer);
 
     if (WiFi.status() == WL_CONNECTED)
     {
 
-        HTTPClient http;
+        BearSSL::WiFiClientSecure client;
+        client.setInsecure();
 
-        http.begin(PATH);
-        http.addHeader("Content-Type", "application/json");
+        HTTPClient https;
 
-        int httpCode = http.POST(buffer);
-        String payload = http.getString();
+        if (https.begin(client, HOST, PORT, PATH)) {
+            https.addHeader("Content-Type", "application/json");
 
-        Serial.println(httpCode);
-        Serial.println(payload);
+            Serial.println(String("POST: ") + PATH);
+            Serial.println(buffer);
 
-        http.end();
+            int httpCode = https.POST(buffer);
+            String payload = https.getString();
+
+            Serial.println(httpCode);
+            Serial.println(payload);
+
+            https.end();
+        } else {
+            Serial.println("Failed to connect to host.");
+        }
     }
     else
     {
         Serial.println("Error in WiFi connection");
     }
 
-    delay(60000);
+    ESP.deepSleep(SLEEP_SECONDS * 1000000);
 }
